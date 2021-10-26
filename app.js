4361ce22eef02e85d527f5c1defb8d79ad0aefa3
SARVESH SINGH <43215686+sarvesh507@users.noreply.github.com>
2021-10-26 23:20:12 +0530

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ if (!config.get('jwtPrivateKey')) {
   process.exit(1);
 }
 
-mongoose.connect('mongodb+srv://sarvesh:sarvesh@cluster0.d5azk.mongodb.net/RoleAuthentication?retryWrites=true&w=majority', { 
+mongoose.connect(process.env.MONGO_URI, { 
   useNewUrlParser: true, 
   useUnifiedTopology: true,
   })
